aa225e39967af2db90af0f6d48d43fda71d8c63a
0leslaw <119787710+0leslaw@users.noreply.github.com>
2024-10-14 17:45:05 +0200

    zmiany

--- microblog-app/microblog/main.py
+++ microblog-app/microblog/main.py
@@ -1,4 +1,5 @@
 import datetime
+import random
 import dateutil.tz
 
 from flask import Blueprint, render_template
@@ -11,16 +12,77 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def index():
-    user = model.User(1, "mary@example.com", "mary")
+    current_user = model.User(1, "current@example.com", "Current Use")
+    user = model.User(2, "mary@example.com", "mary")
     posts = [
         model.Post(
-            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal())
+            1, user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
         ),
         model.Post(
-            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal())
+            2, user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
         ),
         model.Post(
-            3, user, "Yet another post", datetime.datetime.now(dateutil.tz.tzlocal())
+            3, user, "Yet another post", datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
         ),
     ]
-    return render_template("main/index.html", posts=posts)
+    return render_template("main/index.html", posts=posts, current_user=current_user)
+
+@bp.route("/profile_view")
+def profile_view():
+    current_user = model.User(1, "mary@example.com", "mary", """description (should be limited)
+                dslcslcpsel,c;el, iufe weoj wef weoifjwe ijofwej
+                we fowojfweoj sfoiwefoijwe oifweoi wefoijwe ijofwejwe j
+                w ifew jowhufhuwehou fweihu fwehuoifwehf
+                w ef weouf owefouhwefou wefoh weohu fweo joiwejoi """)
+    posts = [
+        model.Post(
+            1, current_user, "Test post", datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
+        ),
+        model.Post(
+            2, current_user, "Another post", datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
+        ),
+        model.Post(
+            3, current_user, "Yet another post", datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
+        ),
+    ]
+    return render_template("main/profile_view.html", posts=posts, current_user=current_user)
+
+@bp.route("/post_view")
+def post_view():
+    data = makeDummyDataTask5Lab3_1()
+    return render_template("main/post_view.html", post=data['main_post'], posts=data['responses'])
+
+def makeDummyDataTask5Lab3_1():
+    posts = []
+    users = []
+    lorem = " At vero eos et accusamus et iusto odio dignissimos ducimus, qui blanditiis praesentium voluptatum deleniti atque corrupti, quos dolores et quas molestias"
+    ran = random.randint(50, 100)
+    for i in range(10):
+        users.append(
+            model.User(
+                i, f"user{i}@gmail.com", f"user{i}", f"user{i}"+lorem[:random.randint(50, 100)]
+            )
+        )
+    for i in range(1,10):
+        lor = lorem[:ran]
+        posts.append(
+            model.Post(
+                    i,
+                    users[i],
+                    f"user{i}"+lorem,
+                    datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M'),
+                    0
+                )
+            )
+        ran = random.randint(50, 100)
+        
+        
+    main_post = model.Post(
+                    0,
+                    users[0],
+                    "user0"+lorem[:ran],
+                    datetime.datetime.now(dateutil.tz.tzlocal()).strftime('%Y-%m-%d %H:%M')
+                )
+        
+    return {'main_post': main_post, 'responses': posts}
+    
--- microblog-app/microblog/model.py
+++ microblog-app/microblog/model.py
@@ -1,13 +1,15 @@
 class Post:
-    def __init__(self, post_id, user, text, timestamp):
+    def __init__(self, post_id, user, content, timestamp, answered_to_id=None):
         self.post_id = post_id
         self.user = user
-        self.text = text
+        self.content = content
         self.timestamp = timestamp
+        self.answered_to_id = answered_to_id
 
 
 class User:
-    def __init__(self, user_id, email, name):
+    def __init__(self, user_id, email, name, desc=None):
         self.user_id = user_id
         self.email = email
         self.name = name
+        self.desc = desc
